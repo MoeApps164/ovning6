c0f381dd7b2d6ebb084f12974fefedd58254fdd5
Moe <mohammed.kaldi@outlook.com>
2025-12-09 14:13:50 +0100

    Första Selenium testet – startar Chrome och öppnar Demo Web Shop

---
+++ src/main/java/se/moe/ovning6/FörstaTestet1.java
@@ -0,0 +1,33 @@
+package se.moe.ovning6;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class FörstaTestet1 {
+
+    public static void main(String[] args) {
+
+        // 1. Ställ in ChromeDriver automatiskt
+        WebDriverManager.chromedriver().setup();
+
+        // 2. Starta webbläsaren Chrome
+        WebDriver driver = new ChromeDriver();
+
+        // 3. Gå till en webbsida (Demo Web Shop)
+        driver.get("https://demowebshop.tricentis.com/");
+
+        // 4. Hämta sidans titel och skriv ut i IntelliJ
+        System.out.println("Sidtitel: " + driver.getTitle());
+
+        // 5. Pausa 3 sekunder så du ser sidan
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        // 6. Stäng Chrome
+        driver.quit();
+    }
+
+}
