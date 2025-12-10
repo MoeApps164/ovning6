0d947548426264b67d7a94ecd2a9d3a04684ac55
Moe <mohammed.kaldi@outlook.com>
2025-12-10 21:40:05 +0100

    Steg 1–2: Locator By.id – hittade element via ID, skrev ut tagName och testade sökrutan

--- src/main/java/se/moe/ovning6/FörstaTestet1.java
+++ src/main/java/se/moe/ovning6/FörstaTestet1.java
@@ -2,6 +2,8 @@ package se.moe.ovning6;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 
 public class FörstaTestet1 {
 
@@ -15,13 +17,22 @@ public class FörstaTestet1 {
 
         // 3. Gå till en webbsida (Demo Web Shop)
         driver.get("https://demowebshop.tricentis.com/");
+        WebElement element = driver.findElement(By.id("dialog-notifications-success"));
+        WebElement element1 = driver.findElement(By.id("small-searchterms"));
+        System.out.println("Element hittat: " + element.getTagName());
+        System.out.println("Element hittat: " + element1.getTagName());
+
+
+
+
+
 
         // 4. Hämta sidans titel och skriv ut i IntelliJ
         System.out.println("Sidtitel: " + driver.getTitle());
 
         // 5. Pausa 3 sekunder så du ser sidan
         try {
-            Thread.sleep(3000);
+            Thread.sleep(5000);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
